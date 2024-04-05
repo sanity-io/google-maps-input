5cea2c9854fa024c2f3c421bdc058216fffe7940
Bjørge Næss <bjoerge@gmail.com>
2024-04-05 18:14:17 +0200

    fix(deps): upgrade @sanity/ui and require styled-components v6 as peer dependency (#68)
    * fix(deps): upgrade @sanity/ui and require styled-components v6 as peer dependency
    
    * fixup! fix(deps): upgrade @sanity/ui and require styled-components v6 as peer dependency

--- src/input/GeopointInput.styles.tsx
+++ src/input/GeopointInput.styles.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import {styled} from 'styled-components'
 
 export const PreviewImage = styled.img`
   width: 100%;
--- src/map/Map.styles.tsx
+++ src/map/Map.styles.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import {styled} from 'styled-components'
 
 export const MapContainer = styled.div`
   position: absolute;
